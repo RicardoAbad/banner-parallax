228b451e53a807f7fe099a06d28143fe793bbd69
RicardoAbad <gambiloko@gmail.com>
2015-12-26 14:13:53 +0100

    Added section 2 to the html whit images, css and javascript. Implementation of jquery.fullPage as navigation system (scroll based)

--- js/code.js
+++ js/code.js
@@ -1,19 +1,39 @@
 $(function() {
 
+
 	if ($(window).width() < 481) {
-  	$('#scene').parallax( { limitY:1 } );
+		$('#scene').parallax( { limitY:1 } );
 	} else{
 		$('#scene').parallax( { limitY:20 } );
 	};
-  
 
 
-	$(window).on('scroll', function(event) {
+	$('#scene-2').parallax();
+	
 
-		var wScroll = $(window).scrollTop();
+
+	$('#main-container').fullpage({
+		scrollingSpeed: 5000,
+		resize:true,
+		//scrollBar: true,
+		onLeave: function(index, nextIndex, direction){
+			var leavingSection = $(this);
+			//after leaving section 1
+			if (index == 1 && direction =='down') {
+				console.log("Going to section 2!");
+				$('#page-1').addClass('out');
+			} else if (index == 2 && direction == 'up') {
+				console.log("Going to section 1!");
+				$('#page-1').removeClass('out');
+			}
+		}
+	});
+
+	/*$(window).on('scroll', function(event) {
+
+		var wScroll = $('#page-1').scrollTop();
 		var wHeight = $(window).height();
 		var opacity = 1 - (wScroll/800); 
-
 		// Simulating parallax scroll
 		$('.sitename div').css({
 			'-webkit-transform': 'translateY('+ (wScroll / 2)*2.25 +'px) scale('+ ( 1+(wScroll / 800) ) +')',
@@ -36,40 +56,34 @@ $(function() {
 			'-ms-transform': 'translateX(-'+ (wScroll / 2)*1.1 +'px) translateY('+ (wScroll / 1.3) +'px) scale('+ ( 1-(wScroll / 1000) ) +')',
 			transform: 'translateX(-'+ (wScroll / 2)*1.1 +'px) translateY('+ (wScroll / 1.3) +'px) scale('+ ( 1-(wScroll / 1000) ) +')',
 		});
-	});
+	});*/
 
 
 	// Scroll to section smoth SLOOOOWWWWWWWLLLLLYYYYYY
-	$('.goto-slow').on('click', function(event) {
+	/*$('.goto-slow').on('click', function(event) {
 		event.preventDefault();
 		var $anchor = $(this);
 		var pos_ancla = $($anchor.attr('href')).offset().top;
 		$('html, body').stop().animate({
 			scrollTop: pos_ancla
 		}, 8000);
-	});
+	});*/
 	// Scroll to section smoth
-	$('.goto').on('click', function(event) {
+	/*$('.goto').on('click', function(event) {
 		event.preventDefault();
 		var $anchor = $(this);
 		var pos_ancla = $($anchor.attr('href')).offset().top;
 		$('html, body').stop().animate({
 			scrollTop: pos_ancla
 		}, 2000);
-	});
+	});*/
 
 	// Inicio descenso automático condicional y temporizado
+	/*setTimeout(function(){
+		$.fn.fullpage.moveSectionDown();
+	}, 5000);
 	setTimeout(function(){
-		var wScroll = $(window).scrollTop();
-		var ancla = $('#blue-section').offset().top;
-		if (wScroll > 0) {
-			return false;
-		} else{
-		  $('html, body').stop().animate({
-				scrollTop: ancla
-			}, 15000);
-		};
-	}, 15000);
-
+		$.fn.fullpage.moveSectionUp();
+	}, 15000);*/
 
 });

11a95a58a470d8148e429026b964c6feb42c99a7
RicardoAbad <gambiloko@gmail.com>
2016-02-16 08:39:26 +0100

    Added SmoothScroll and updated other files

--- README.md
+++ README.md
@@ -6,8 +6,17 @@ What's that ?
 
 But in this point, i think that *can be useful for a variety of pourposes*, so i go to work in it one more deep level...
 
-![enter image description here](http://recursos.yoytuweb.hol.es/banner-parallax/images/aldeaweb_com.jpg)
+![imagen estática del resultado](http://recursos.yoytuweb.hol.es/banner-parallax/images/aldeaweb_com.jpg)
 
-The demo page is [here](http://recursos.yoytuweb.hol.es/banner-parallax/banner.html) , where you can see in action the base idea.
+The demo page is <a href="http://recursos.yoytuweb.hol.es/banner-parallax/banner.html" target="_blank">banner.html</a> , where you can see in action the base idea.
 
 
+**Added styl file** to use Stylus preprocessor.
+
+I use node.js with this command *(add -c to compress the css ouput and autoprefixer-stylus --with "{ browsers: ['ie 7', 'ie 8'] }" if you want specific options to use in the especific plugin)*:
+
+```bash
+stylus -u nib -u autoprefixer-stylus --with "{ browsers: ['last 8 versions','ie 7', 'ie 8'] }" -w banner.styl -o ../css/
+```
+
+Also, in Windows &amp; Mac you have a free tool called **PrePros**, and Mac users have **CodeKit**. Those are tools used to preprocess diferent languajes as *jade, sass, scss, stylus, etc...*.
--- js/code.js
+++ js/code.js
@@ -3,31 +3,152 @@ $(function() {
 
 	if ($(window).width() < 481) {
 		$('#scene').parallax( { limitY:1 } );
-	} else{
+	} else {
 		$('#scene').parallax( { limitY:20 } );
+	};
+	$('#scene-2').parallax();
+	$('#scene-3').parallax();
+	$('#scene-3b').parallax();
+	
+
+	if ( $('.page-index').length > 0 ) {
+		$('#main-container').fullpage({
+			scrollingSpeed: 4000,
+			resize:false,
+			scrollBar: true,
+			//fitToSection: false,
+			fixedElements: '#controls',
+			responsiveHeight: 676,
+			onLeave: function(index, nextIndex, direction){
+				sound_slide();
+				var leavingSection = $(this);
+				//after leaving section 1
+				if (index == 1 && direction =='down') {
+					console.log("Going to section 2!");
+					$('#page-1').addClass('out');
+					$('.mano, .cat').removeClass('out');
+				} else if (index == 2 && direction == 'up') {
+					console.log("Going to section 1!");
+					$('#page-1').removeClass('out');
+					$('.mano, .cat').addClass('out');
+				}
+				//after leaving section 2
+				else if (index == 2 && direction =='down') {
+					console.log("Going to section 3!");
+					$('.mano').addClass('out');
+					$('.cat').addClass('out-down');
+				} else if (index == 3 && direction == 'up') {
+					console.log("Going to section 2!");
+					$('.mano').removeClass('out');
+					$('.cat').removeClass('out-down');
+				}
+				//after leaving section 3
+				else if (index == 3 && direction =='down') {
+					console.log("Going to section 4!");
+					$('.link-1,.link-2,.link-3,.link-4').removeClass('out');
+				} else if (index == 4 && direction == 'up') {
+					console.log("Going to section 3!");
+					$('.link-1,.link-2,.link-3,.link-4').addClass('out');
+				}
+			}
+		});
+	} else if ( $('.page-sobre-mi').length > 0 ) {
+		$('#main-container').fullpage({
+			scrollingSpeed: 4000,
+			resize:false,
+			scrollBar: true,
+			//fitToSection: false,
+			easingcss3:'cubic-bezier(0.300, -0.315, 0.715, 1.315)',
+			fixedElements: '#controls',
+			responsiveHeight: 676,
+			onLeave: function(index, nextIndex, direction){
+				sound_slide();
+				var leavingSection = $(this);
+				//actuando en la 2
+				if (index == 3 && direction =='down' || index == 5 && direction == 'up') {
+					console.log("Going to section 4!");
+					$('#page-3c').removeClass('out');
+				} else {
+					console.log("Leaving section 4!");
+					$('#page-3c').addClass('out');
+				}
+				//actuando en la 5
+				if (index == 7 && direction =='down') {
+					console.log("Going to section 8!");
+					$('.link-1,.link-2,.link-3,.link-4').removeClass('out');
+				} else if (index == 8 && direction == 'up') {
+					console.log("Going to section 7!");
+					$('.link-1,.link-2,.link-3,.link-4').addClass('out');
+				}
+			}
+		});
+	};
+
+	$('.next').on('click', function(event) {
+		$.fn.fullpage.moveSectionDown();
+	});
+	$('.prev').on('click', function(event) {
+		$.fn.fullpage.moveSectionUp();
+	});
+
+
+	//audio 
+	var index_audio;
+	if ( $('.page-index').length > 0 ) {
+		var index_audio = document.getElementById('soundtrack_index');
+		index_audio.volume=0.75;
+	} else if ( $('.page-sobre-mi').length > 0 ) {
+		var index_audio = document.getElementById('sondtrack_sobre_mi');
+	};
+
+	$('#audio_control').on('click', function(event) {
+		if (index_audio.paused ) {
+			index_audio.play();
+			$('#audio_control').addClass('vol-off');
+			$('#audio_control').removeClass('vol-on');
+		} else {
+			index_audio.pause();
+			$('#audio_control').addClass('vol-on');
+			$('#audio_control').removeClass('vol-off');
+		}
+	});
+
+	$('a').mouseenter(function() {
+		document.getElementById('hover_off').volume=0.2;
+		if (!index_audio.paused ) {
+			document.getElementById('hover_off').play();
+		}
+	});
+	$('a').mouseleave(function() {
+		document.getElementById('hover').volume=0.17;
+		if (!index_audio.paused ) {
+			document.getElementById('hover').play();
+		}
+	});
+
+	function sound_slide() {
+		document.getElementById('slide_sound').volume=0.2;
+		if (!index_audio.paused ) {
+			document.getElementById('slide_sound').play();
+		}
 	};
 
 
-	$('#scene-2').parallax();
-	
 
 
-	$('#main-container').fullpage({
-		scrollingSpeed: 5000,
-		resize:true,
-		//scrollBar: true,
-		onLeave: function(index, nextIndex, direction){
-			var leavingSection = $(this);
-			//after leaving section 1
-			if (index == 1 && direction =='down') {
-				console.log("Going to section 2!");
-				$('#page-1').addClass('out');
-			} else if (index == 2 && direction == 'up') {
-				console.log("Going to section 1!");
-				$('#page-1').removeClass('out');
+
+	function moveBg() {
+		$('.bg-slide').animate({'background-position': (init_pos *10) + 'px'},1000,'linear',function(){
+				init_pos++;
+				moveBg();
 			}
-		}
-	});
+		);
+	};
+
+	if ( $('.bg-slide').length > 0 ) {
+		var init_pos = 0;
+		moveBg();
+	};
 
 	/*$(window).on('scroll', function(event) {
 
@@ -77,7 +198,6 @@ $(function() {
 			scrollTop: pos_ancla
 		}, 2000);
 	});*/
-
 	// Inicio descenso automático condicional y temporizado
 	/*setTimeout(function(){
 		$.fn.fullpage.moveSectionDown();
